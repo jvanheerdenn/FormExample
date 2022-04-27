94d3ade26811af3f0068beab6c221b1b7ae9be69
jvanheerdenn <jvanheerden@vectoritcgroup.com>
2022-04-27 10:04:41 +0200

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,20 +4,22 @@ import { FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
 
-  constructor(private formbuilder: FormBuilder){}
+  constructor(private formbuilder: FormBuilder) {}
 
   form = this.formbuilder.group({
     input1: ['', Validators.required],
     input2: ['', Validators.required],
-  })
+  });
 
-  getData(){
+  getData(buttonName: string) {
     const val1 = this.form.get('input1').value;
     const val2 = this.form.get('input2').value;
+    console.log(`val1 desde ${buttonName}: ${val1}`);
+    console.log(`val2 desde ${buttonName}: ${val2}`);
   }
 }
